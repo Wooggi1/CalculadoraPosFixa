dc2879536a5682dbbeb6cc2890e30c1bde57b1df
Igor Brandão <111445872+Punkrig@users.noreply.github.com>
2023-11-30 11:12:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,51 +5,64 @@
 #include <math.h>
 #include <string.h>
 
-int main() {
+int main()
+{
     char entrada[100];
     printf("Digite a expressao: ");
-    fgets(entrada, 100, stdin); 
+    fgets(entrada, 100, stdin);
 
     Pilha *pilhaOperandos = criarPilha();
 
-    for (int i = 0; entrada[i] != '\0'; i++) {
-        if (isdigit(entrada[i]) || (entrada[i] == '.' && isdigit(entrada[i + 1]))) {
+    // Mover a declaração da variável result para o início da função main
+    double result;
+
+    for (int i = 0; entrada[i] != '\0'; i++)
+    {
+        if (isdigit(entrada[i]) || (entrada[i] == '.' && isdigit(entrada[i + 1])))
+        {
             double operando = strtod(&entrada[i], NULL);
-            while (isdigit(entrada[i]) || entrada[i] == '.') {
+            while (isdigit(entrada[i]) || entrada[i] == '.')
+            {
                 i++;
             }
             empilhar(pilhaOperandos, operando);
-        } else if (isalpha(entrada[i])) {
+        }
+        else if (isalpha(entrada[i]))
+        {
             float operando = desempilhar(pilhaOperandos);
-            char funcao[4] = {0}; 
+            char funcao[4] = {0};
             int j = 0;
-            while (isalpha(entrada[i]) && j < 3) {
+            while (isalpha(entrada[i]) && j < 3)
+            {
                 funcao[j++] = entrada[i++];
             }
             empilhar(pilhaOperandos, aplicarFuncao(funcao, operando));
-            i--;  
-        } else if (entrada[i] == '+' || entrada[i] == '-' || entrada[i] == '*' || entrada[i] == '/' || entrada[i] == '^') {
-            
+            i--;
+        }
+        else if (entrada[i] == '+' || entrada[i] == '-' || entrada[i] == '*' || entrada[i] == '/' || entrada[i] == '^')
+        {
+            // Remover a declaração da variável funcao2, pois ela não é usada
             float operando2 = desempilhar(pilhaOperandos);
             float operando1 = desempilhar(pilhaOperandos);
 
-            switch (entrada[i]) {
-                case '+':
-                    empilhar(pilhaOperandos, operando1 + operando2);
-                    break;
-                case '-':
-                    empilhar(pilhaOperandos, operando1 - operando2);
-                    break;
-                case '*':
-                    empilhar(pilhaOperandos, operando1 * operando2);
-                    break;
-                case '/':
-                    empilhar(pilhaOperandos, operando1 / operando2);
-                    break;
-                case '^':
-                    double result = pow(operando2, operando1);
-                    empilhar(pilhaOperandos, result);
-                    break;
+            switch (entrada[i])
+            {
+            case '+':
+                empilhar(pilhaOperandos, operando1 + operando2);
+                break;
+            case '-':
+                empilhar(pilhaOperandos, operando1 - operando2);
+                break;
+            case '*':
+                empilhar(pilhaOperandos, operando1 * operando2);
+                break;
+            case '/':
+                empilhar(pilhaOperandos, operando1 / operando2);
+                break;
+            case '^':
+                result = pow((double)operando1, (double)operando2);
+                empilhar(pilhaOperandos, result);
+                break;
             }
         }
     }
